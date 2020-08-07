468f7c1882148b94b9357dcf7910f9e9cc8d0582
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-08-07 17:52:00 +0530

    conditions

--- app.py
+++ app.py
@@ -1,11 +1,120 @@
-weight = int(input("weight: "))
-unit = input("(L)bs or (K)gs: ")
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"You are {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"You are {converted} pounds")
+# weight = int(input("weight: "))
+# unit = input("(L)bs or (K)gs: ")
+# if unit.upper() == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos")
+# else:
+#     converted = weight / 0.45
+#     print(f"You are {converted} pounds")
+
+# course = 'learning python course'
+# print('learning' in course)
+
+# Roundof
+# t = 2.765
+# print(round(t))
+
+# Absolute will return positive value
+# t = -2.333
+# print(abs(t))
+
+# using math operator module
+#
+# import math
+#
+# t = 4
+# print(math.ceil(t))
+# print(math.floor(t))
+# print(math.factorial(t))
+# print(math.isqrt(t))
+
+# if statements
+
+'''Price of a house is $1M
+    if buyer has good credit they need to
+    put down 10%
+    otherwise
+    they need to put down 20%
+    print the down payment'''
+
+
+# price = 1000000
+#
+# has_good = False
+#
+# if has_good:
+#     down_payment = 0.1*price
+# else:
+#     down_payment = 0.2*price
+#
+# print(f'Down payment: ${down_payment}')
+
+
+
+# has_high_income = True
+# has_good_credit = True
+#
+# if has_high_income and has_good_credit:
+#     print('Eligible for loan')
+# else:
+#     print('Sorry, your not eligible')
+
+
+# comparison operators
+
+# temperature = 20
+#
+# if (temperature>30):
+#     print('it is hot day')
+# elif(temperature<10):
+#     print('it is cold day')
+# else:
+#     print('its neither hot nor cold')
+
+# name = 40
+#
+# if name<3:
+#     print('name must be three characters')
+# elif name>50:
+#     print('name cab be maximum of 50 characters')
+# else:
+#     print('name looks good')
+#
+
+#  Weight convertor
+
+
+# x = int(input('Weight: '))
+# y = input('Weight in (L)bs or (K)g: ')
+#
+# if y.upper() == 'L':
+#     converted = x * 0.45
+#     print(f'you weight is: {converted} in kilos')
+# else:
+#     converted = x / 0.45
+#     print(f'your weight is: {converted} in pounds')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
